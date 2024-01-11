feefaefb09dfa8ce11510d3d394d2b6f39766773
Ethan <linesh.simpcity@gmail.com>
2024-01-11 15:25:16 +0800

    ♻️refactor: inline app.tsx into index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { createRoot, type Root } from 'react-dom/client'
+import { Provider as ReduxProvider } from 'react-redux'
 
-import { App } from './app'
 import { Envs } from './constants/envs'
+import { AppRoutes } from './app-routes'
+import { configureAppStore } from './configure-store'
 
 if (process.env.NODE_ENV === Envs.DEVELOPMENT) {
   // setup mock server
   // setup other dev environment stuffs
 }
 
+const store = configureAppStore()
 const root: Root = createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(
+  <React.StrictMode>
+    <ReduxProvider store={store}>
+      <AppRoutes />
+    </ReduxProvider>
+  </React.StrictMode>
+)
