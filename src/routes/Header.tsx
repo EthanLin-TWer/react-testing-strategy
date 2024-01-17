90b812bdf987a537c31d40856c37b44b05a7345c
Ethan <linesh.simpcity@gmail.com>
2024-01-17 18:16:22 +0800

    ✅test: increase coverage - remove unused component

--- src/routes/Header.tsx
+++ src/routes/Header.tsx
@@ -4,11 +4,9 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
 import { Outlet } from 'react-router-dom'
-import { HeaderComponent } from '../business-components/shared/header/HeaderComponent'
 
 export const Header = () => (
   <>
-    {/* <HeaderComponent />*/}
     <Outlet />
   </>
 )
